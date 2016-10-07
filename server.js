aa76687fb826568c15b32159ce7b2e284383167e
Bryan Choe <bryan.choe88@gmail.com>
2016-10-06 17:40:40 -1000

    css displayed

--- server.js
+++ server.js
@@ -15,20 +15,50 @@ http.createServer((request, response) => {
   //console.log('response: ', response);
 
 let url = request.url;
+//console.log("url********",url);
 let show;
-  if (request.method === "GET"){
-    fs.readFile(`./public/index.html`, (err, data) => {
-      if(err){
+let splitUrl = url.split(".")[1];
+console.log('splitURL********:',splitUrl);
+  if(splitUrl !== "css"){
+    if (request.method === "GET"){
+      fs.readFile(`./public/${url}`, (err, data) => {
+        if(err){
 
-        console.log("err********:", err);
-        //console.error(err);
+          console.log("err********:", err);
+          //console.error(err);
+        } else {
+          //console.log('data: ', data.toString());
+          show = data.toString();
+          //console.log(show);
+          response.writeHead(200, {
+           "Content-Type": "text/html",
+           "Content-Length":show.length
+          });
+          //res.setHeader('Content-Type', 'text/html');
+          response.write(show);
+          response.end();
+        }
+      });
+    }
+  } else {
+    fs.readFile(`./css/styles.css`, (err, data) => {
+      if(err){
+      console.log("err********:", err);
+      //console.error(err);
       } else {
         //console.log('data: ', data.toString());
         show = data.toString();
-        console.log(show);
+        //console.log(show);
         //res.setHeader('Content-Type', 'text/html');
-        response.end(show);
+        //console.log("css******:", show);
+        response.writeHead(200, {
+           "Content-Type": "text/css",
+           "Content-Length":show.length
+         });
+        response.write(show);
+        response.end();
       }
+
     });
   }
 
